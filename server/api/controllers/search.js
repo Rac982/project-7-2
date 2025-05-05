3b24a14303d748f2f57877b1cc9a1090aa92b8fd
alexdant91 <alexdant91@gmail.com>
2025-05-05 18:04:18 +0200

    fix(search): include user parameter in search query validation

--- server/api/controllers/search.js
+++ server/api/controllers/search.js
@@ -9,19 +9,18 @@ const { Category, Product } = require('../../db');
  * @permission User
  */
 const searchCateogriesAndProducts = async (req, res) => {
-    const user = req.user;
-
     const schema = Joi.object().keys({
+        u: Joi.string().required(),
         q: Joi.string().required(),
     });
 
     try {
         const data = await schema.validateAsync(req.query);
 
-        const { q } = data;
+        const { u, q } = data;
 
-        const categories = await Category.find({ user, name: { $regex: q, $options: 'i' } }, null, { lean: true });
-        const products = await Product.find({ user, name: { $regex: q, $options: 'i' } }, null, { lean: true })
+        const categories = await Category.find({ user: u, name: { $regex: q, $options: 'i' } }, null, { lean: true });
+        const products = await Product.find({ user: u, name: { $regex: q, $options: 'i' } }, null, { lean: true })
             .populate('category')
             .populate('labels');
 
