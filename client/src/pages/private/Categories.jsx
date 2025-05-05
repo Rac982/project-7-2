bd41efa45f150ab7fd063cff1daebcad2cef85a7
Rac982 <rachele982@gmail.comm>
2025-05-05 19:08:43 +0200

    fix

--- client/src/pages/private/Categories.jsx
+++ client/src/pages/private/Categories.jsx
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import CustomImage from '../../components/shared/CustomImage'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import SearchInput from '../../components/shared/SearchInput'
 import { clearSearchResult } from '../../store/slices/searchSlice'
+import { setCurrentCategory } from '../../store/slices/categorySlice'
 
 const Categories = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { all: categories } = useSelector((state) => state.categories);
     const { products: filteredProducts } = useSelector((state) => state.search);
     const [query, setQuery] = useState("")
 
     const isSearching = filteredProducts.length > 0;
+
+    const handleNavigate = (categoryId) => {
+        const activeCategory = categories.find((item) => item._id == categoryId);
+        dispatch(setCurrentCategory(activeCategory));
+        navigate(`/private/products/${categoryId}`);
+    };
 
     // Reset dei risultati quando la ricerca viene svuotata
     useEffect(() => {
@@ -35,10 +43,10 @@ const Categories = () => {
                 <div className="px-5 py-4 space-y-3">
                     {isSearching ? (
                         filteredProducts?.map((product) => (
-                            <Link
-                                to={`/private/products/${product.categoryId}`}
+                            <div
+                                onClick={handleNavigate}
                                 key={product._id}
-                                className="block relative bg-white rounded-3xl shadow-elevation-1 p-4 mb-[2.5rem] min-h-[5.9375rem] flex items-center justify-between transition-all active:scale-[0.98]"
+                                className="cursor-pointer block relative bg-white rounded-3xl shadow-elevation-1 p-4 mb-[2.5rem] min-h-[5.9375rem] flex items-center justify-between transition-all active:scale-[0.98]"
                             >
                                 <div className="max-w-[10.1875rem]">
                                     <div className="flex-1 pr-3">
@@ -53,14 +61,14 @@ const Categories = () => {
                                         </p>
                                     </div>
                                 </div>
-                            </Link>
+                            </div>
                         ))
                     ) : (
                         categories.map((category) => (
-                            <Link
-                                to={`/private/products/${category._id}`}
+                            <div
+                                onClick={() => handleNavigate(category._id)}
                                 key={category._id}
-                                className="block relative bg-white rounded-3xl shadow-elevation-1 p-4 mb-[2.5rem] min-h-[5.9375rem] flex items-center justify-between transition-all active:scale-[0.98]"
+                                className="cursor-pointer block relative bg-white rounded-3xl shadow-elevation-1 p-4 mb-[2.5rem] min-h-[5.9375rem] flex items-center justify-between transition-all active:scale-[0.98]"
                             >
                                 <div className="max-w-[10.1875rem]">
                                     <div className="flex-1 pr-3">
@@ -77,7 +85,7 @@ const Categories = () => {
                                     alt={category.name}
                                     className="absolute top-[-1.25rem] right-[1rem] w-[9.75rem] h-[9.75rem] sm:w-[8rem] sm:h-[8rem] object-contain z-10"
                                 />
-                            </Link>
+                            </div>
                         ))
                     )}
                 </div>
