21ef377fbcdf3d42878c9ee4a7a063e78ba9d178
JavaTota <jomigone@gmail.com>
2025-05-20 09:26:09 -0500

    Improve layout alignment in NavbarBusinessLogin for better visual consistency

--- client/src/components/dashboard/NavbarBusinessLogin.jsx
+++ client/src/components/dashboard/NavbarBusinessLogin.jsx
@@ -3,8 +3,8 @@ import CustomImage from "../shared/CustomImage";
 
 const NavbarBusinessLogin = () => {
   return (
-    <nav className="flex gap-2 w-full bg-white shadow-md p-2 h-[100px] items-center">
-      <div className="max-w-[1440px] flex justify-end w-full">
+    <nav className="flex gap-2 w-full bg-white shadow-md p-2 h-[100px] items-center justify-center">
+      <div className="max-w-[1440px] flex justify-end w-full items-center">
         <Link to="/business">
           <CustomImage
             src="/images/business_images/Logo-TableLink.jpg"
